0c3bdfebfb9c0ea34e45d79f0b7d0d155f68ca18
Ronald Ormea <rormeasdev@gmail.com>
2025-11-08 23:17:48 +0100

    feature:Navbar

---
+++ app/(root)/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+
+import Logo from '@/components/Shared/logo/logo'
+import React from 'react'
+import NavBarDesktop from './NavBarDesktop/NavBarDesktop'
+import NavBarMobile from './NavBarMobile/NavBarMobile'
+
+export  function Navbar() {
+  return (
+    <nav className='w-[90%] md:w-full md:max-w-5xl mx-auto sticky
+     top-5 z-20 flex items-center justify-between py-3 px-6
+      bg-purple-500/30  backdrop-blur-md rounded-full shadow-lg border border-purple-400/20'>
+        <Logo/> 
+        <NavBarDesktop/>
+        <NavBarMobile/>
+    </nav>
+  )
+}
+
+
---
+++ app/(root)/page.tsx
@@ -0,0 +1,21 @@
+import { Navbar } from "./components";
+
+
+
+
+
+
+export default function Home() {
+  return (
+    
+    <div className="min-h-screen w-full bg-linear-to-br from-slate-900 via-purple-900 to-slate-900">
+      
+      <Navbar />
+
+
+
+
+    </div>
+     
+  );
+}
